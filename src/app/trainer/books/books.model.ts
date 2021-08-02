a9c0e75b1ea3f903d7c8910c73cabbc8dbc1ad3b
SandraVinod <62558403+SandraVinod@users.noreply.github.com>
2021-08-02 18:28:14 +0530

    Delete src/app/trainer/books directory

--- src/app/trainer/books/books.model.ts
+++
@@ -1,9 +0,0 @@
-export class BooksModel{
-    constructor(
-        public title?:String,
-        public author?:String,
-        public genre?:String,
-        public img?:File,
-        public _id?:any
-    ){};
-}
