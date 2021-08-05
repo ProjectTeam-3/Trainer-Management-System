b341c827ec51fee85c3e121217d531d5538018df
Sandra V <sandravinod98@gmail.com>
2021-08-05 22:26:43 +0530

    aboutus

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarEvent, CalendarEventAction, CalendarView } from 'angular-calendar';
-import {
- 
-  ChangeDetectionStrategy,
-  ViewChild,
-  TemplateRef,
-} from '@angular/core';
-import { timeStamp } from 'console';
+;
+
 import { startOfDay } from 'date-fns';
 import { AuthService } from '../auth.service';
 import { TrainerProfileService } from '../trainer-profile.service';
@@ -21,10 +16,10 @@ import { Subject } from 'rxjs';
 export class ScheduleComponent implements OnInit {
   
   constructor(private calender:TrainerProfileService,private gettoken:AuthService,private modal: NgbModal) { }
-  // @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
+
   schedules:any=[];
  items:CalendarEvent[]=[]
-//  actions:CalendarEventAction[]=[];
+
   ngOnInit(): void {
     var email=localStorage.getItem('email')
     this.calender.getSchedule(email)
@@ -42,7 +37,7 @@ export class ScheduleComponent implements OnInit {
           start:startOfDay(new Date(this.schedules[i].startdate)),
           end:new Date(this.schedules[i].enddate),
           title: 'course id:' +this.schedules[i].courseid,
-          // actions:this.actions
+          
         })
        console.log(this.events);
          this.events=this.items;
@@ -60,31 +55,7 @@ export class ScheduleComponent implements OnInit {
     this.view = view;
   }
    events: CalendarEvent[]=[]
-  // modalData: {
-  //   action: string;
-  //   event: CalendarEvent;
-  // };
-  // handleEvent(action: string, event: CalendarEvent): void {
-  //   this.modalData = { event, action };
-  //   this.modal.open(this.modalContent, { size: 'lg' });
-  // }
-  // actions: CalendarEventAction[] = [
-  //   {
-  //     label: '<i class="fas fa-fw fa-pencil-alt"></i>',
-  //     a11yLabel: 'Edit',
-  //     onClick: ({ event }: { event: CalendarEvent }): void => {
-  //       this.handleEvent('Edited', event);
-  //     },
-  //   },
-  //   {
-  //     label: '<i class="fas fa-fw fa-trash-alt"></i>',
-  //     a11yLabel: 'Delete',
-  //     onClick: ({ event }: { event: CalendarEvent }): void => {
-  //       this.events = this.events.filter((iEvent) => iEvent !== event);
-  //       this.handleEvent('Deleted', event);
-  //     },
-  //   },
-  // ];
+ 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     console.log(date);
     
